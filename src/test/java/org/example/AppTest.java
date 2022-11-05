e236a96202cddd6009fb0cf61cc0d9e1c7362fa7
volodink <volodin.konstantin@gmail.com>
2022-11-05 23:27:26 +0300

    Initial commit

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import jdk.jfr.Name;
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+    public List lst;
+
+    @BeforeAll
+    public static void printMessage() {
+        System.out.println("Test Message!");
+    }
+
+    @BeforeEach
+    public void printEachMessage() {
+        System.out.println("Test Each Case!");
+        lst = new ArrayList<Integer>();
+    }
+
+   @Test
+   @DisplayName("Add something")
+   @RepeatedTest(100)
+   @Timeout(value = 100, unit = TimeUnit.MILLISECONDS)
+   void add() {
+       List expectedList = new ArrayList<Integer>();
+       assertEquals(lst, expectedList);
+   }
+
+}
---
+++ src/test/java/org/example/entity/UserTest.java
@@ -0,0 +1,12 @@
+package org.example.entity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserTest {
+    @Test
+    void testUser() {
+        assertEquals(1, 1);
+    }
+}
